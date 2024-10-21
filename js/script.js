4d4e646bd446faf525b4e24156d9574bf4e8e4f3
martinanggah <sadewamarga@gmail.com>
2024-10-22 02:26:03 +0700

    Java Script Form

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-// ini java
+
+function replacename(){
+    let nameuser = prompt("Hai... Siapakah nama anda?","");
+    document.getElementById("nama-user").innerHTML = nameuser
+}
+
+document.getElementById("ganti-nama").addEventListener("click", function(){replacename()})
+// replacename();
+
+function tampilkanData() {
+    var namapesan = document.getElementById("nama-pesan").value;
+    var tanggalLahir = document.getElementById("tanggalLahir").value;
+    var jenisKelamin = document.querySelector('input[name="jenisKelamin"]:checked').value;
+    var pesan = document.getElementById("pesan").value;
+
+    // var hasilDiv = document.getElementById("hasil-pesan");
+    // hasilDiv.innerHTML = 
+    // "<p>Nama: " + namapesan + 
+    // "</p><p>Tanggal Lahir: " + tanggalLahir + 
+    // "</p><p>Jenis Kelamin: " + jenisKelamin + 
+    // "</p><p>Pesan: " + pesan + "</p>";
+
+    document.getElementById("hasil-pesan").innerHTML = 
+    "<p>Nama: " + namapesan + 
+    "</p><p>Tanggal Lahir: " + tanggalLahir + 
+    "</p><p>Jenis Kelamin: " + jenisKelamin + 
+    "</p><p>Pesan: " + pesan + "</p>";
+}
